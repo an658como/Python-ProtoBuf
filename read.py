e1738e9a13c6bdd6977f0ddedef60bc270f61d03
Ali Naseri <an658@ad.cam.ac.uk>
2022-05-31 10:35:35 +0100

    some minor changes compared to previous commit

--- read.py
+++ read.py
@@ -5,11 +5,12 @@ import todolist_pb2 as TodoList
 # Open the file and convert the serialized string to a regular human readable block of data
 my_list = TodoList.TodoList()
 
+'''
 with open("./serializedFile", "rb") as fd:
     my_list.ParseFromString(fd.read())
 
 print(my_list)
-
+'''
 
 with open("./test.pb", "rb") as fd:
     my_list.ParseFromString(fd.read())
